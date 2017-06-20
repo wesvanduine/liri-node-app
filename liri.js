83150d8f34216c6da6ab5129312f149a58651161
Wes Van Duine <Wes Van Duine>
2017-06-19 21:43:31 -0700

    added if statement for my-tweet

--- liri.js
+++ liri.js
@@ -9,6 +9,16 @@ var keys = require('./keys.js');
 var argument = process.argv[2];
 
 
+
+
+//////////////////////////////////
+//           Logic			    //
+//////////////////////////////////
+
+//TWITTER//
+//////////////////////////////////
+
+//variable to grab the Twitter API
 var client = new Twitter({
     consumer_key: keys.consumer_key,
     consumer_secret: keys.consumer_secret,
@@ -16,9 +26,6 @@ var client = new Twitter({
     access_token_secret: keys.access_token_secret
 });
 
-//////////////////////////////////
-//           Logic			    //
-//////////////////////////////////
 //Twitter paramaters to search username coderdev4056, return only the last 20 tweets, and will not trim the username so the username will come up with the username and not ID#
 var params = {
     screen_name: "coderdev4056",
@@ -26,16 +33,17 @@ var params = {
     trim_user: false
 }
 
+//function to get user data, error check, loop through the length of the tweets and log each one with a datestamp
 
-//Twitter logic
-client.get('/statuses/user_timeline.json', params, function(error, data, response) {
-    if (!error) {
-        var tweets = data;
-
-        console.log("User Name: " + params.screen_name + " last 20 tweets");
-        for (i = 0; i < tweets.length; i++) {
-            console.log(tweets[i].created_at);
-            console.log(tweets[i].text);
-        }
-    } else console.log(error);
-});
+if (argument === 'my-tweets') {
+    client.get('/statuses/user_timeline.json', params, function(error, data, response) {
+        if (!error) {
+            var tweets = data;
+            console.log("User Name: " + params.screen_name + " last 20 tweets");
+            for (i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log(tweets[i].text);
+            }
+        } else console.log(error);
+    })
+};
