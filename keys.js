98732e73510c32d1e0c15e9d1a66c6144278c2ea
Wes Van Duine <Wes Van Duine>
2017-06-19 21:36:28 -0700

    twitter logic

---
+++ keys.js
@@ -0,0 +1,8 @@
+/*console.log('this is loaded');*/
+
+module.exports = {
+  consumer_key: '6XTtrsNlqU4SYRHhrXacKXWF8',
+  consumer_secret: 'kNpE9q2hNEDohupzrthADJJ9ZZTpGeTONThIY5nTyBKeQ017Nh',
+  access_token_key: '876547669392375810-F6XCOF3bhjazYsELqNNI2mQqPZ63LAA',
+  access_token_secret: 'CNFeDLjcRxD1HRq8OxPPd4YWbhiW0EMoByJhHmzY1Ryi2',
+}
--- liri.js
+++ liri.js
@@ -4,31 +4,38 @@
 
 var fs = require('fs');
 var request = require('request');
-var twitter = require('twitter');
+var Twitter = require('twitter');
 var keys = require('./keys.js');
 var argument = process.argv[2];
 
 
+var client = new Twitter({
+    consumer_key: keys.consumer_key,
+    consumer_secret: keys.consumer_secret,
+    access_token_key: keys.access_token_key,
+    access_token_secret: keys.access_token_secret
+});
 
+//////////////////////////////////
+//           Logic			    //
+//////////////////////////////////
 //Twitter paramaters to search username coderdev4056, return only the last 20 tweets, and will not trim the username so the username will come up with the username and not ID#
 var params = {
     screen_name: "coderdev4056",
     count: 20,
-    trim_user: false,
+    trim_user: false
 }
 
 
 //Twitter logic
-if (argument === 'my-tweets') {
-    twitter.find('statuses/user_timeline', params, gotData);
-
-    function gotData(error, data, response) {
+client.get('/statuses/user_timeline.json', params, function(error, data, response) {
+    if (!error) {
         var tweets = data;
-        for (var i = 0; i < tweets.length; i++) {
-            console.log(tweets[i].text);
-            console.log(tweets[i].created_at);
-        }
 
-    };
-    outputText();
-}
+        console.log("User Name: " + params.screen_name + " last 20 tweets");
+        for (i = 0; i < tweets.length; i++) {
+            console.log(tweets[i].created_at);
+            console.log(tweets[i].text);
+        }
+    } else console.log(error);
+});
