435b5aee1d5dcdf333b99e0a933778431249daec
Wes Van Duine <Wes Van Duine>
2017-06-23 17:57:02 -0700

    twitter & start spotify

--- keys.js
+++ keys.js
@@ -1,8 +1,17 @@
-/*console.log('this is loaded');*/
+console.log('this is loaded');
 
-module.exports = {
+exports.twitterKeys = {
   consumer_key: '6XTtrsNlqU4SYRHhrXacKXWF8',
   consumer_secret: 'kNpE9q2hNEDohupzrthADJJ9ZZTpGeTONThIY5nTyBKeQ017Nh',
   access_token_key: '876547669392375810-F6XCOF3bhjazYsELqNNI2mQqPZ63LAA',
   access_token_secret: 'CNFeDLjcRxD1HRq8OxPPd4YWbhiW0EMoByJhHmzY1Ryi2',
+}
+
+exports.spotifyKeys = {
+	client_ID: '99da4bad56fa48168dc0deb9ef273b41',
+	client_secret: '30c50c4da992414097186b0edd1aa6e4'
+}
+
+exports.omdbKey = {
+	
 }
--- liri.js
+++ liri.js
@@ -1,51 +1,65 @@
 //////////////////////////////////
-//         VARIABLES			//
+//         VARIABLES            //
 //////////////////////////////////
-
-var fs = require('fs');
-var request = require('request');
 var Twitter = require('twitter');
 var keys = require('./keys.js');
-var argument = process.argv[2];
-
+var Spotify = require('node-spotify-api');
+var request = require('request');
 
 //////////////////////////////////
 //           TWITTER            //
 //////////////////////////////////
+var getTweet = function() {
+    var client = new Twitter(keys.twitterKeys);
 
-//variable to grab the Twitter API
-var client = new Twitter({
-    consumer_key: keys.consumer_key,
-    consumer_secret: keys.consumer_secret,
-    access_token_key: keys.access_token_key,
-    access_token_secret: keys.access_token_secret
-});
-
-//Twitter paramaters to search username coderdev4056, return only the last 20 tweets, and will not trim the username so the username will come up with the username and not ID#
-var params = {
-    screen_name: "coderdev4056",
-    count: 20,
-    trim_user: false
-}
-
-//function to get user data, error check, loop through the length of the tweets and log each one with a datestamp
-if (argument === 'my-tweets') {
-    client.get('/statuses/user_timeline.json', params, function(error, data, response) {
+    var params = { screen_name: 'coderdev4056' };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
-            var tweets = data;
-            console.log("User Name: " + params.screen_name + " last 20 tweets");
+            /*console.log(tweets);*/
             for (i = 0; i < tweets.length; i++) {
                 console.log(tweets[i].created_at);
+                console.log(' ');
                 console.log(tweets[i].text);
             }
-        } else console.log(error);
-    })
-};
+        }
+    });
+}
 
 //////////////////////////////////
 //           Spotify            //
 //////////////////////////////////
-if (argument === 'spotify-this-song'){
-	var songTitle = process.argv[3];
+var spotify = new Spotify({
+  id: "6c4df8acd26343bc81ac66dc1e9d6675",
+  secret: "4a9d71ef882e4c01b64d9ee33055264b"
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
 
-}
+
+//////////////////////////////////
+//        switch statement      //
+//////////////////////////////////
+var userInput = function(caseData, functionData) {
+    switch (caseData) {
+        case 'my-tweets':
+            getTweet();
+            break;
+        default:
+            console.log('LIRI does not understand')
+    }
+};
+
+var runThis = function(argOne, argTwo) {
+    userInput(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
+
+
+
